2298440b79980d225764c7c8b4a157379ab96160
Juan José Molano <molano.juan.2629@eam.edu.co>
2026-03-01 17:19:59 -0500

    Añade register

--- frontend/src/app/features/auth/register/register.ts
+++ frontend/src/app/features/auth/register/register.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Api } from '../../../core/services/api';
+import { Card } from '../../../shared/card/card';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, Card],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrl: './register.scss'
 })
-export class Register {}
+export class Register {
+
+  credentials = { nombre: '', correo: '', edad: '', contrasena: '' };
+
+  constructor(private api: Api, private router: Router) { }
+
+  register() {
+    this.api.register(this.credentials).subscribe({
+      next: (res: any) => {
+        this.router.navigate(['/login']);
+      },
+      error: () => alert('Register failed')
+    });
+  }
+
+}
