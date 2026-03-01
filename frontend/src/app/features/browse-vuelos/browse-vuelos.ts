c3f8dd59f67c7d72a6a8559726d96a9470160935
Juan José Molano <molano.juan.2629@eam.edu.co>
2026-03-01 17:21:32 -0500

    Añade browse vuelos

---
+++ frontend/src/app/features/browse-vuelos/browse-vuelos.ts
@@ -0,0 +1,42 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Usuario } from '../../models/usuario';
+import { Api } from '../../core/services/api';
+import { Card } from '../../shared/card/card';
+import { Button } from '../../shared/button/button';
+import { Vuelo } from '../../models/vuelo';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-browse-vuelos',
+  imports: [Card, CommonModule, Button, RouterLink],
+  templateUrl: './browse-vuelos.html',
+  styleUrl: './browse-vuelos.scss',
+})
+export class BrowseVuelos implements OnInit {
+
+  vuelos: Vuelo[] | null = null;
+
+  constructor(private api: Api, private cdr: ChangeDetectorRef, private router: Router) { }
+
+  ngOnInit() {
+    this.api.getVuelos().subscribe({
+      next: (data) => {
+        this.vuelos = data;
+        this.cdr.detectChanges();
+      },
+      error: () => console.error('Failed to load vuelos')
+    });
+  }
+
+  selectedId: number | null = null;
+
+  selectVuelo(id: number) {
+    this.selectedId = id;
+    console.log('Selected vuelo ID:', id);
+  }
+
+  goToDetails(id: number) {
+    this.router.navigate(['/vuelo', id]);
+  }
+}
