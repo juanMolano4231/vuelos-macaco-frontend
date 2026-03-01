4e94f859eb0bee70b83ae2c4d0a32ae9a4b8c221
Juan José Molano <molano.juan.2629@eam.edu.co>
2026-03-01 17:18:49 -0500

    Añade api y rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Dashboard } from './features/dashboard/dashboard';
+import { HomeButtons } from './features/auth/home-buttons/home-buttons';
+import { Register } from './features/auth/register/register';
+import { BrowseVuelos } from './features/browse-vuelos/browse-vuelos';
+import { BrowseTiquetes } from './features/browse-tiquetes/browse-tiquetes';
+import { VueloDetail } from './features/vuelo-detail/vuelo-detail';
+import { TiqueteDetail } from './features/tiquete-detail/tiquete-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'home', component: HomeButtons },
+    { path: 'vuelos', component: BrowseVuelos },
+    { path: 'tiquetes', component: BrowseTiquetes },
+    { path: 'vuelo/:id', component: VueloDetail },
+    { path: 'tiquete/:id', component: TiqueteDetail }
+];
--- frontend/src/app/core/services/api.ts
+++ frontend/src/app/core/services/api.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Usuario } from '../../models/usuario';
 import { Vuelo } from '../../models/vuelo';
 import { Tiquete } from '../../models/tiquete';
 
 @Injectable({ providedIn: 'root' })
 export class Api {
-  private url = 'http://backend:3000'; // Docker network
+  private url = 'http://localhost:3000/api';
   private token = '';
 
   constructor(private http: HttpClient) { }
   setToken(t: string) { this.token = t; }
   headers() { return { headers: new HttpHeaders({ Authorization: `Bearer ${this.token}` }) }; }
 
-  login(c: any) { return this.http.post(`${this.url}/login`, c); }
-  register(u: any) { return this.http.post(`${this.url}/register`, u); }
+  login(c: any) { return this.http.post(`${this.url}/auth/login`, c); }
+  register(u: any) { return this.http.post(`${this.url}/auth/register`, u); }
   getMe() { return this.http.get<Usuario>(`${this.url}/me`, this.headers()); }
   getMyTickets() { return this.http.get<Tiquete[]>(`${this.url}/me/tickets`, this.headers()); }
   getTickets() { return this.http.get<Tiquete[]>(`${this.url}/tickets`, this.headers()); }
